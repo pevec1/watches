3748ff89fb61c7ea7f16c6e7122aab493b774a57
Andrei Harenkov <pevec1@yandex.ru>
2024-03-04 14:19:25 +0300

    update 2024-03-04 v0.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import Clock from "./components/Clock";
+import moment from "moment-timezone";
 //import dataForm from "./../data.json";
 function App() {
-  
-  const [dataList, setList] = useState([])
+   const [dataList, setList] = useState([]);
   const [data, setData] = useState({
     date: new Date().toLocaleDateString(),
     traveled: 0,
   });
-    console.log(data);
- 
+  console.log(data);
+
   const onSubmit = (event) => {
     event.preventDefault();
 
@@ -17,47 +18,60 @@ function App() {
     const formData = new FormData(target);
 
     const data2 = Object.fromEntries(formData);
-    if (dataList.find((dat)=> dat.date == data2.date)) {
+    if (dataList.find((dat) => dat.date == data2.date)) {
       setList((dataList) =>
-        dataList
-          .filter((dat, i, arr) => {
-            if (dat.date == data2.date)
-              dat.traveled = Number(dat.traveled) + Number(data2.traveled);
-            return arr;
-          })
+        dataList.filter((dat, i, arr) => {
+          if (dat.date == data2.date) dat.traveled = Number(data2.traveled);
+          return arr;
+        })
       );
-    } 
-    else
-    setList((dataList) =>
-      [...dataList, data2].sort((a, b) => {
-        return (
-          new Date(a.date.slice(0, 10).split(".").reverse().join("-")) -
-          new Date(b.date.slice(0, 10).split(".").reverse().join("-"))
-        );
-      })
-    );
-        console.log(data2, dataList);
+    } else
+      setList((dataList) =>
+        [...dataList, data2].sort((a, b) => {
+          return (
+            new Date(a.date.slice(0, 10).split(".").reverse().join("-")) -
+            new Date(b.date.slice(0, 10).split(".").reverse().join("-"))
+          );
+        })
+      );
+    console.log(data2, dataList);
   };
   const onChange = (event) => {
     const { target } = event;
-    console.log(target)
+    console.log(target);
     const { value } = target;
     setData(value);
-    console.log("value:",value);
+    console.log("value:", value);
   };
   const handleClick = (x) => {
-    setList(dataList=>[...dataList].filter(( obj )=> {
-    return obj.date !== x;
-}))
+    setList((dataList) =>
+      [...dataList].filter((obj) => {
+        return obj.date !== x;
+      })
+    );
     console.log(dataList, x);
-  }
+  };
+
+  //******************************************** */
+
+  const clock = moment().format("LTS").toString(); //new Date().toLocaleTimeString();
+  console.log(clock);
+  const [time, setTime] = useState(Date.now());
+
+useEffect(() => {
+  const interval = setInterval(() => setTime(Date.now()), 1000);
+  return () => {
+    clearInterval(interval);
+  };
+}, []);
 
   return (
     <div className="wrapper">
+      <Clock clock={clock} />
       <form onSubmit={onSubmit}>
         <div className="row">
           <div className="column">
-            <label htmlFor="date">Дата (ДД.ММ.ГГГГ)</label>
+            <label htmlFor="date">Название</label>
             <input
               type="text"
               name="date"
@@ -66,7 +80,7 @@ function App() {
             />
           </div>
           <div className="column">
-            <label htmlFor="traveled">Пробег, км</label>
+            <label htmlFor="traveled">Временная зоча (ч)</label>
             <input
               type="text"
               name="traveled"
@@ -84,7 +98,15 @@ function App() {
           <ul>
             {[...dataList].map((record, i) => (
               <li key={i}>
-                <span>{record.date}</span> <span>{record.traveled}</span> <button className="delete" onClick={()=>handleClick(record.date)}>удалить</button>
+                <span>{record.date}</span>
+                <Clock clock={clock} hours={record.traveled} />{" "}
+                <span>{record.traveled} часа </span>{" "}
+                <button
+                  className="delete"
+                  onClick={() => handleClick(record.date)}
+                >
+                  удалить
+                </button>
               </li>
             ))}
           </ul>
